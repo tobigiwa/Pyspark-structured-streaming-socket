7575756c451b826fd3d8906a33aec77c0b71fcc5
OluwatobiTobias <giwaoluwatobi@gmail.com>
2022-07-22 13:58:07 +0100

    updating pyspark_socket_streaming.py and server_socket.py

--- pyspark_socket_streaming.py
+++ pyspark_socket_streaming.py
@@ -1,11 +1,13 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode, split
 import socket
+import sys
 
 server = socket.socket()
 host =  socket.gethostbyname(socket.gethostname())
 
 try:
-    spark = SparkSession.builder.appName('boo').master('local[*]').getOrCreate()
+    spark = SparkSession.builder.appName('socket_streaming').master('local[*]').getOrCreate()
 
     try:  
         socket_source_stream = spark \
@@ -15,21 +17,28 @@ try:
         .option('port', '5011') \
         .option('includeTimestamp', True).load()
     except Exception as err:
-        print(f'Error establishing spark socket stream : \n',err)
+        print(f'Error establishing spark socket stream : \n', sys.exc_info())
     else:
-        print('-----\n Is spark streaming: ', socket_source_stream.isStreaming, '\n\n')   
+        print('\n----- Is spark streaming: ', socket_source_stream.isStreaming, '\n')   
         socket_source_stream.printSchema()
-
-        socket_sink_stream = socket_source_stream.writeStream.queryName('sneeze')\
+        try:
+            stream_words_df = socket_source_stream.select(explode(split(socket_source_stream.value, " ")).alias("words"))
+            stream_words_count_df = stream_words_df.groupBy("words").count()
+        except Exception as err:
+            print('Error in transformation:\n', sys.exc_info())
+        else:
+            socket_sink_stream = stream_words_count_df.writeStream.queryName('sink_socket_streaming')\
             .format('console')\
-            .trigger(processingTime='2 second')\
+            .outputMode('update')\
             .start(truncate=False)
-        socket_sink_stream.awaitTermination(30)         
+            socket_sink_stream.awaitTermination(55)    #.trigger(processingTime='1 second')\ "I removed this since processing power is hardware-dependant, since we ain't streaming much adding it neccesarily won't break our code, just make our terminal a little dirty"
+        finally:
+            pass      
     finally:
         print('\nStreaming Over.\n\n')
 
 except Exception as err:
-    print(f'\Error creating SparkSession: \n\n', err)
+    print(f'\Error creating SparkSession: \n\n', sys.exc_info())
 
 else:
     print('All went good.\n')
@@ -37,3 +46,4 @@ else:
 finally:
     spark.stop()
     print('SparkSession stopped!')
+
--- server_socket.py
+++ server_socket.py
@@ -1,36 +1,46 @@
 import socket
 import time
+import sys
 
 host = socket.gethostbyname(socket.gethostname())
 port = 5011
 flag = True
 
-try:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket_obj:
-        server_socket_obj.bind((host, port))
-        server_socket_obj.listen(5)
-        print('Open to received connection...\n')
+def replacing_punctuations(words:str):
+    a  = {',': '',
+        '.': ''}
+    for key, value in a.items():
+        words = words.replace(key, value, -1).lower()
+    return words
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket_obj:
+    server_socket_obj.bind((host, port))
+    server_socket_obj.listen(5)
+    print('Open to receiving connection...\n')
+    try:
         conn_obj, addr = server_socket_obj.accept()  # we did'nt add this in the `whille loop` as mostly seen since we are expecting just one connection
         print(F'Got connection from {addr}.\n')
 
         while flag:
             with conn_obj:
                 with open('files/text_file.txt', 'r') as file_handler:
-                    for lines in file_handler:
-                        conn_obj.send(bytes(f'{lines}', 'utf-8'))
-                        time.sleep(3)
+                    for (i,lines) in enumerate(file_handler):
+                        lines = replacing_punctuations(lines)
+                        print('batch', i,lines)     
+                        conn_obj.send(bytes(lines, 'utf-8'))
+                        time.sleep(5)
                     else:
                         print('End of file(EOF) reached.\n')
-                        flag = False # we we got here our text file is already exhausted
+                        flag = False # if we got here our text file is already exhausted
 
-        server_socket_obj.shutdown(socket.SHUT_RDWR)
-        # our context manager would manage the `server_socket_obj.close()`
+    except Exception and KeyboardInterrupt  as err: 
+        print('Error occured when connecting or sending data to the client \n', sys.exc_info())
+        server_socket_obj.shutdown(socket.SHUT_RDWR)   # our context manager would manage the `server_socket_obj.close()`
+        sys.exit()
 
-except socket.error as err:
-    print('\n\nSocket error: \n', err)
-
-else:
-    print('All went good...\n')
-
-finally:
-    print('Server connection successfully shutdown.')
+    else:
+        server_socket_obj.shutdown(socket.SHUT_RDWR)  # our context manager would manage the `server_socket_obj.close()
+        print('All went good...\n')
+        
+    finally:
+        print('Server connection successfully closed.')
